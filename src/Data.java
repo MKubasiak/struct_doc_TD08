cd507ff9e7a493227cbc43effd0e9f27dfbae5fa
Charlotte Fernandes <fernandescharlotte54@gmail.com>
2018-04-05 19:43:01 +0200

    Exercice 2
    Manque question 4
    Revoir la méthode d'insertion et suppression pour les tracks

--- src/Data.java
+++ src/Data.java
@@ -1,11 +1,13 @@
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class Data {
 
     private String key = "6c1897c3409c621a1522760e7164fbf6";
     private String url ="http://ws.audioscrobbler.com/";
     private Document doc = new Document();
-
 
     /**
      * Méthode qui permet de récupérer les informations d'un album au format json
@@ -16,7 +18,6 @@ public class Data {
         HTTPTools httpTools = new HTTPTools();
         String jsonAlbum = httpTools.sendGet(url+"2.0/?method=album.getinfo&api_key="+key+"&artist="+artiste+"&album="+album+"&format=json");
         Document docAlbum = doc.parse(jsonAlbum);
-        //System.out.println(docAlbum.toString());
         return docAlbum;
     }
 
@@ -29,8 +30,31 @@ public class Data {
         HTTPTools http = new HTTPTools();
         String jsonTrack = http.sendGet(url+"2.0/?method=track.getInfo&api_key="+key+"&artist="+artiste+"&track="+title+"&format=json");
         Document docTrack = doc.parse(jsonTrack);
-        //System.out.println(docTrack.toString());
         return docTrack;
+    }
+
+    /**
+     * Méthode qui permet d'inserer un document à la base de données
+     * @param doc document bson à insérer
+     * @param colName nom de la collection dans laquelle doit être inséré le document
+     */
+    public void insertDocument(Document doc, String colName){
+        MongoCollection<Document> collection = Main.database.getCollection(colName);
+        collection.insertOne(doc);
+    }
+
+    /**
+     * Méthode qui permet de supprimer un document à la base de données
+     * @param mbid mbid permettant d'identifier le document à supprimer
+     * @param colName nom de la collection dans laquelle sera supprimé le document
+     */
+    public void deleteDocument(String mbid, String colName){
+        MongoCollection collection = Main.database.getCollection(colName);
+        if(colName == "albums"){
+            collection.deleteMany(eq("album.mbid", mbid));
+        }else{
+            collection.deleteMany(eq("track.mbid", mbid));
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Data album = new Data();
-        album.getDataAlbum("Cher", "Believe");
+    public static MongoClient mongo;
+    public static MongoDatabase database;
 
-        Data title = new Data();
-        title.getDataTitle("Cher", "Believe");
-        
+    public static Document docAlbum = new Document();
+    public static Document docTrack = new Document();
+
+    public final static String DB = "musicdb";
+    public final static String COLALBUMS = "albums";
+    public final static String COLTRACKS = "tracks";
+
+    public static void main(String[] args) {
+        // Creating a Mongo client
+        mongo = new MongoClient( "localhost" , 27017 );
+
+        // Accessing the database
+        database = mongo.getDatabase(DB);
+
+        Data dataAlbum = new Data();
+        docAlbum = dataAlbum.getDataAlbum("Cher", "Believe");
+        //dataAlbum.insertDocument(docAlbum, COLALBUMS);
+        //dataAlbum.deleteDocument("63b3a8ca-26f2-4e2b-b867-647a6ec2bebd",COLALBUMS);
+
+        Data dataTrack = new Data();
+        docTrack = dataTrack.getDataTitle("Cher", "Believe");
+        /***** A RETESTER ******/
+        //dataTrack.insertDocument(docTrack, COLTRACKS);
+        //dataTrack.deleteDocument("63b3a8ca-26f2-4e2b-b867-647a6ec2bebd",COLTRACKS);*/
+
+
     }
 }
